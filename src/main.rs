78df68a1d10dded39832801b8ec9fb6dbd54f04c
Katherine Watson <katie@radicalsemiconductor.com>
2021-11-21 19:53:02 -0800

    Add nice little clean run logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use hex;
-use base64;
-use hex::FromHexError;
-use std::collections::HashMap;
+use std::env;
 use std::str::from_utf8;
+use std::collections::HashMap;
 
+use base64;
+use hex;
+use hex::FromHexError;
+
+const WILD_PENALTY: f32 = 10.0;
 const NUM_LETTERS: usize = 26;
 const LETTER_FREQS: [f32; NUM_LETTERS] = [
     0.082, 0.015, 0.028, 0.043, 0.13, 0.022, 0.02, 0.061, 0.07, 0.0015,
@@ -11,10 +14,28 @@ const LETTER_FREQS: [f32; NUM_LETTERS] = [
     0.028, 0.0098, 0.024, 0.0015, 0.02, 0.00074
 ];
 
-const WILD_PENALTY: f32 = 10.0;
+
+// Challenge 1: Convert hex to base64
 
 fn hex_to_base64(s: &str) -> Result<String, FromHexError> {
     Ok(base64::encode(hex::decode(s)?))
+}
+
+
+fn challenge_hex_to_base64() {
+    let hex_val = "49276d206b696c6c696e6720796f757220627261696e206c\
+                   696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let result = hex_to_base64(hex_val).unwrap();
+    println!("{}", result);
+}
+
+// Challenge 2: Fixed XOR
+
+fn challenge_fixed_xor() {
+    let a_val = "1c0111001f010100061a024b53535009181c";
+    let b_val = "686974207468652062756c6c277320657965";
+    let result = xor(a_val, b_val).unwrap();
+    println!("{}", result);
 }
 
 fn xor(a: &str, b: &str) -> Result<String, FromHexError> {
@@ -24,6 +45,20 @@ fn xor(a: &str, b: &str) -> Result<String, FromHexError> {
     let xor_bytes: Vec<u8> = byte_pairs.map(|(a_byte, b_byte)| 
         a_byte ^ b_byte).collect();
     Ok(hex::encode(xor_bytes))
+}
+
+// Challege 3: Single-byte XOR cipher
+
+#[derive(Debug)]
+enum CrackError {
+    ParseError(FromHexError),
+    NoSolutionError
+}
+
+impl From<FromHexError> for CrackError {
+    fn from(error: FromHexError) -> Self {
+        Self::ParseError(error)
+    }
 }
 
 fn get_letter_counts(s: &str) -> HashMap<char, u32> {
@@ -58,18 +93,6 @@ fn score_sentence(s: &str) -> f32 {
     score
 }
 
-#[derive(Debug)]
-enum CrackError {
-    ParseError(FromHexError),
-    NoSolutionError
-}
-
-impl From<FromHexError> for CrackError {
-    fn from(error: FromHexError) -> Self {
-        Self::ParseError(error)
-    }
-}
-
 fn crack_one_byte_xor(s: &str) -> Result<String, CrackError>{
     let s_bytes = hex::decode(s)?;
 
@@ -94,45 +117,22 @@ fn crack_one_byte_xor(s: &str) -> Result<String, CrackError>{
     best_msg
 }
 
-
-
-fn main() {
-    println!("{}", crack_one_byte_xor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap());
+fn challenge_single_byte_xor() {
+    let msg = "1b37373331363f78151b7f2b783431333d\
+               78397828372d363c78373e783a393b3736";
+    let result = crack_one_byte_xor(msg).unwrap();
+    println!("{}", result);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Problem selection logic
 
-    #[test]
-    fn crack_one_byte_xor_success() {
-        let msg = "1b37373331363f78151b7f2b783431333d\
-                   78397828372d363c78373e783a393b3736";
-        assert_eq!(crack_one_byte_xor(msg).unwrap(),
-                   String::from("Cooking MC's like a pound of bacon"));
-        
-    }
-
-    #[test]
-    fn hex_to_base64_success() {
-        let hex_val = "49276d206b696c6c696e6720796f757220627261696e206c\
-                       696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        let base64_val = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBs\
-                          aWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-        assert_eq!(hex_to_base64(hex_val).unwrap(), String::from(base64_val));
-    }
-
-    #[test]
-    fn hex_to_base64_fail() {
-        let hex_val = "49324af";
-        assert_eq!(hex_to_base64(hex_val), Err(FromHexError::OddLength));
-    }
-
-    #[test]
-    fn xor_success() {
-        let a_val = "1c0111001f010100061a024b53535009181c";
-        let b_val = "686974207468652062756c6c277320657965";
-        let xor_val = "746865206b696420646f6e277420706c6179";
-        assert_eq!(xor(a_val, b_val).unwrap(), String::from(xor_val));
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let problem_num = &args[1].parse::<i32>().unwrap();
+    match problem_num {
+        1 => challenge_hex_to_base64(),
+        2 => challenge_fixed_xor(),
+        3 => challenge_single_byte_xor(),
+        _ => println!("No such problem!")
     }
 }
